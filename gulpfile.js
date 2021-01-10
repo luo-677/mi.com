2ad5447d2f1ce38b7ef8449cb917d9a0eb2b899a
luo-677 <luolan9527@gmail.com>
2021-01-10 16:49:23 +0800

    mi.com

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const rename = require('gulp-rename');
 const htmlmin = require('gulp-htmlmin');
 const cssmin = require('gulp-cssmin');
+const jsmin = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
 // 部署任务
 gulp.task('print', function() {
     return new Promise((resolve, reject) => {
@@ -60,9 +62,9 @@ gulp.task('jsmin', function() {
 // gulp-imagemin
 // $ cnpm i gulp-imagemin -D
 gulp.task('imagemin', function() {
-    return gulp.src('./src/img/*')
+    return gulp.src('./top_ad.jpg')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/img'));
+        .pipe(gulp.dest('./src'));
 });
 
 // 5. 合并文件
